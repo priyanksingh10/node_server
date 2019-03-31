dcd97ff75c6264d5684762c04f0f33a07252ca0e
Priyank Singh <priyank.singh01@gmail.com>
2019-03-30 23:54:51 -0700

    Integrated with MongoDB

--- server.js
+++ server.js
@@ -3,9 +3,15 @@ const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
+const { MongoClient } = require('mongodb');
 const SETTINGS = require('./env-vars');
 
 const app = express();
+
+// Connection URL
+const url = 'mongodb://localhost:27017';
+
+MongoClient.connect(url, { useNewUrlParser: true });
 
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -16,8 +22,12 @@ app.use('/js', express.static(path.join(__dirname, 'node_modules/popper.js/dist'
 app.set('views', path.join(__dirname, 'src/views'));
 app.set('view engine', 'ejs');
 
+const taskRouter = require('./src/router/taskRouter')();
+
+app.use('/task', taskRouter);
+
 app.get('/', (req, res) => {
-  res.render('index', { title: 'My first EJS page' });
+  res.render('index', { title: 'What are you upto?' });
 });
 
 app.listen(SETTINGS.port, () => {
---
+++ src/router/taskRouter.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const debug = require('debug')('app:task');
+const { MongoClient } = require('mongodb');
+
+const taskRouter = express.Router();
+
+// Connection URL
+const url = 'mongodb://localhost:27017';
+
+// Database Name
+const dbName = 'matrix';
+
+
+function router() {
+  taskRouter.route('/').get((req, resp) => {
+    (async function mongo() {
+      let client;
+      try {
+        client = await MongoClient.connect(url);
+        const db = client.db(dbName);
+        const response = await db.collection('tasks').find().toArray();
+        resp.render('task-list', { title: 'ToDos', data: response });
+      } catch (e) {
+        debug(e.stack);
+      }
+      client.close();
+    }());
+  });
+
+  taskRouter.route('/add').get((req, resp) => {
+    const tasks = [{
+      name: 'Task1',
+      description: 'This is dummy task 1',
+      status: 'In Progress',
+      createdOn: new Date(),
+      priority: 1,
+      completedOn: null
+    }, {
+      name: 'Task2',
+      description: 'This is dummy task 2',
+      status: 'In Progress',
+      createdOn: new Date(),
+      priority: 1,
+      completedOn: null
+    }];
+
+    (async function mongo() {
+      let client;
+      try {
+        client = await MongoClient.connect(url);
+        const db = client.db(dbName);
+        const response = await db.collection('tasks').insertMany(tasks);
+        resp.json(response);
+      } catch (e) {
+        debug(e.stack);
+      }
+      client.close();
+    }());
+  });
+
+  taskRouter.route('/history').get((req, resp) => {
+    resp.render('task-history', { title: 'Your history page' });
+  });
+
+  return taskRouter;
+}
+
+module.exports = router;
